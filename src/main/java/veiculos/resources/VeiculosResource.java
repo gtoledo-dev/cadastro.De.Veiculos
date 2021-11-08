847f2af168ff42c03bd4abd15f0fb55ae6c24108
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-11-08 15:50:45 -0300

    Criado pacote service e arquitetura Restful

--- src/main/java/veiculos/resources/VeiculosResource.java
+++ src/main/java/veiculos/resources/VeiculosResource.java
@@ -1,21 +1,66 @@
 package veiculos.resources;
 
-import java.time.Instant;
+import java.net.URI;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import veiculos.entities.Veiculos;
+import veiculos.services.VeiculosService;
 
 @RestController
 @RequestMapping(value = "/veiculos")
 public class VeiculosResource {
-
+	
+	@Autowired
+	private VeiculosService service;
+	
 	@GetMapping
-	public ResponseEntity<Veiculos> findAll(){
-		Veiculos vc = new Veiculos(1L, "Vectra", "Chevrolet", 2010, "gasolina", "Sim", Instant.parse("2010-07-07T10:03:53z"), Instant.parse("2011-07-07T09:00:53z"));
-		return ResponseEntity.ok().body(vc);
+	public ResponseEntity<List<Veiculos>> findAll(){
+		List<Veiculos> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Veiculos> findById(@PathVariable Long id){
+		Veiculos obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Veiculos> insert(@RequestBody Veiculos obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Veiculos> updateVeiculos(@PathVariable Long id, @RequestBody Veiculos obj){
+		obj = service.updateVeiculos(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+		
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Veiculos> update(@PathVariable Long id, @RequestBody Veiculos obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
---
+++ src/main/java/veiculos/services/VeiculosService.java
@@ -0,0 +1,65 @@
+package veiculos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import veiculos.entities.Veiculos;
+import veiculos.repositories.VeiculosRepository;
+
+@Service
+public class VeiculosService {
+
+	@Autowired
+	private VeiculosRepository repository;
+
+	public List<Veiculos> findAll() {
+		return repository.findAll();
+	}
+
+	public Veiculos findById(Long id) {
+		Optional<Veiculos> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public Veiculos insert(Veiculos obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Veiculos updateVeiculos(Long id, Veiculos obj) {
+		Veiculos entityVeiculos = repository.getOne(id);
+		updateVeiculos(entityVeiculos, obj);
+		return repository.save(entityVeiculos);
+	}
+
+	private void updateVeiculos(Veiculos entityVeiculos, Veiculos obj) {
+		entityVeiculos.setVeiculo(obj.getVeiculo());
+		entityVeiculos.setMarca(obj.getMarca());
+		entityVeiculos.setAno(obj.getAno());
+		entityVeiculos.setDescricao(obj.getDescricao());
+		entityVeiculos.setVendido(obj.getVendido());
+		entityVeiculos.setCreated(obj.getCreated());
+		entityVeiculos.setUpdated(obj.getUpdated());
+
+	}
+
+	public Veiculos update(Long id, Veiculos obj) {
+		Veiculos entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Veiculos entity, Veiculos obj) {
+		entity.setDescricao(obj.getDescricao());
+		entity.setVendido(obj.getVendido());
+		entity.setUpdated(obj.getUpdated());
+
+	}
+
+}
